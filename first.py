47bc56e5fd3ead58a8f78c3a7ecac813ddf16e8f
basskey19 <rohann@gmail.com>
2025-10-12 20:32:01 +0530

    python1

---
+++ first.py
@@ -0,0 +1,12 @@
+name = "Rohann"
+age = 14
+skill = "good at creative thinking "
+hobbies = "Playing video games and batmittionand also studying "
+DoB = "12/7/2011"
+Favvideogames = "Roblox,Fortnite,Ghost of tsusmia and etc"
+print(f"my name is {name}")
+print(f"my age is {age}")
+print(f"my hobbies are {hobbies}")
+print(f"my Skills are {skill}")
+print(f"my Date Of Birth is {DoB}")
+print(f"my Favourite Games are {name}")
