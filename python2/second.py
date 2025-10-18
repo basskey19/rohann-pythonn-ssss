ba80b7132cf6165b14fe568430c452c372362f00
basskey19 <rohann@gmail.com>
2025-10-18 21:29:27 +0530

    thank you for solving my mistake sir

---
+++ python2/second.py
@@ -0,0 +1,15 @@
+num = int(input("type a number,lets see if u are Lucky "))
+if num % 2 == 0:
+    print(f"{num}sorry you are not lucky")
+else:
+    print(f"{num}you have lady luck on your side")
+
+number=int(input("type a number,lets see if u are Lucky "))
+if number > 50:
+    print("your power level is above %0")
+    if number % 2 == 0:
+         print("STRONG")
+    else:
+        print("ur power level is garbage")
+else: 
+   print("Weak")
